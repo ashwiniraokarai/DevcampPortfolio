ae327a405c6a2c2846b8853c2f1fa3a5bcb8186a
ashwiniraokarai <ashwini@biggerpockets.com>
2019-12-08 00:53:04 -0700

    integrated index action in portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,7 @@
 class PortfoliosController < ApplicationController
+
+  def index
+    @portfolios = Portfolio.all #returns a collection
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  #Ash:resources were set up via rails g resource Portfolio title:string subtitle:string body:text image:text thumb_image:text
   resources :portfolios
-  #home about contact were set up as part of Pages controller (rails g controller Pages home about contact)
+
+  #Ash:home about contact were set up as part of Pages controller (rails g controller Pages home about contact)
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
