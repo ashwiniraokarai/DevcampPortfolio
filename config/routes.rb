f6b3fbac8d7cf95c7589bc57494d908568ee270d
ashwiniraokarai <ashwini@biggerpockets.com>
2019-12-08 17:42:01 -0700

    configured custom routes for /, /about and /contact

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,16 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+
   #Ash: resources encapsulates all the necessary RESTful routes for Blogs scaffold)
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Ash:Setting up the root (/) to hit pages/home page such that it overrides the default "yay, you're on rails" page
+  root to: "pages#home"
+
+  #Ash:Setting up /about and /contact to hit pages/about and pages/contact
+  get "about", to: "pages#about"  #you can specify any string for the route that you want. Intead of "about", you can write anything "asdf"
+  get "contact", to: "pages#contact"
+
 end
