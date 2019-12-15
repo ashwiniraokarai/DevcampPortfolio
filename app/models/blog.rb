4293ce7d64ed88bbbef20a9ae673be5af1953f35
ashwiniraokarai <ashwini@biggerpockets.com>
2019-12-14 22:11:53 -0700

    implemented validations for blogs, skills, and portfolio models

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,7 @@ class Blog < ApplicationRecord
   #See https://github.com/norman/friendly_id
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  #validations
+  validates_presence_of :title, :body   #prevents empty record from being saved (thro create or edit) both via the UI and via the console (Blog.create!())
 end
