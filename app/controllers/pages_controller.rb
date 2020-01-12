9ac742149b85e7389800e31e38427d9e3012c8ae
ashwiniraokarai <ashwini@biggerpockets.com>
2020-01-11 21:46:36 -0700

    probbaly edited some comment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @posts = Blog.includes(:topic) #Blog is the model that came generated via Blog scaffold
+    #Blog is the model that came generated via Blog scaffold
+    #.includes(:topic) will filter only those Blogs that include (have) a topic they each belong to
+    #note that this will fetch all Blogs as long they have associated topic_id column even if the value is nil
+    #therefore filter further only for those Blogs that have a non-nil topic id
+    @posts = Blog.includes(:topic).where.not(topic_id: nil)
+              #@companies = Company.includes(:persons).where(:persons => { active: true } ).all
     @skills = Skill.all #Skill is the model that came generated via Skill model
   end
 
